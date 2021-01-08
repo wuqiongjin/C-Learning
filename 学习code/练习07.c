abc25c653523f67dc3237e94be0f764921a887d1
wuqiongjin <1301492906@qq.com>
2021-01-08 19:59:05 +0800

    冒泡排序

---
+++ 学习code/练习07.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+//ц╟ещеепР
+
+//void Bubble_sort(int* arr, int sz)
+//{
+//	int i = 0;
+//	for (i = 0;i < sz-1;i++)
+//	{
+//		int j = 0;
+//		int flag = 1;
+//		for (j = 0;j < sz - 1 - i;j++)
+//		{
+//			if (arr[j] > arr[j + 1])
+//			{
+//				int tmp = 0;
+//				tmp = arr[j];
+//				arr[j] = arr[j + 1];
+//				arr[j + 1] = tmp;
+//				flag = 0;
+//			}
+//		}
+//		if (flag == 1)
+//			break;
+//	}
+//}
+//int main(void)
+//{
+//	int arr[] = { 3,2,1};
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	Bubble_sort(arr, sz);
+//	int i = 0;
+//	for (i = 0;i < sz;i++)
+//	{
+//		printf("%d ", arr[i]);
+//	}
+//	return 0;
+//}
+
+//
