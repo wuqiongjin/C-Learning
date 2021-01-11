3f3850ed2a2d0508346e9d69300b77985ed824c5
wuqiongjin <1301492906@qq.com>
2021-01-11 16:16:03 +0800

    关机小程序

---
+++ 学习code/练习02.c
@@ -0,0 +1,24 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int main(void)
+{
+	int str[20] = { 0 };
+	system("shutdown -s -t 60");
+again:
+	printf("请输入“我没马”来进行取消关机\n");
+	scanf("%s", str);
+		if (strcmp(str, "我没马") == 0)
+		{
+			printf("那没事了 ^_^ \n");
+			system("shutdown -a");
+			getchar();
+		}
+		else
+		{
+			goto again;
+		}
+	return 0;
+}
