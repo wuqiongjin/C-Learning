999ba105cf4b90289651081b1c1df6e1b57f914b
wuqiongjin <1301492906@qq.com>
2021-02-04 09:28:52 +0800

    re分支和循环(课堂).c

---
+++ 学习code/re分支和循环【课堂】.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+//我憨了		1!+2!+3!+...+10!
+
+//int main()
+//{
+//	int i = 0;
+//	int j = 0;
+//	int sum = 0;
+//	//for (i = 1;i <= 10;i++)
+//	//{
+//	//	int ret = 1;
+//	//	for (j = 1;j <= i;j++)
+//	//	{
+//	//		ret *= j;
+//	//	}
+//	//	sum += ret;
+//	//}
+//	int ret = 1;
+//	for (i = 1;i <= 10;i++)
+//	{
+//		ret *= i;
+//		sum += ret;
+//	}
+//	printf("%d", sum);
+//	return 0;
+//}
+
+//二分查找问题【详解见作业里面最后1道】
+
+//int main()
+//{
+//	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
+//	int k = 7;
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//
+//	return 0;
+//}
+
+//测试char ch【注意，getchar的返回值是int，必须使用int char，否则可能在整形提升过程中出现截断，然后死循环】
+
+//int main()
+//{
+//	char ch;
+//	while ((ch = getchar()) != EOF)
+//	{
+//		putchar(ch);
+//	}
+//	return 0;
+//}
