4a7508692d4ef452efddcece12ad85542108f24f
wuqiongjin <1301492906@qq.com>
2021-03-09 08:13:01 +0800

    预处理

--- 学习code/练习17预处理.c
+++ 学习code/练习17预处理.c
@@ -88,3 +88,91 @@
 //	printf("%d\n", ret);
 //	return 0;
 //}
+
+
+////默认对齐数
+//
+////修改默认对齐数为4
+////此时下面的结构体就是以编译器的默认对齐数为4来进行内存对齐的
+//#pragma pack(4)
+//struct S
+//{
+//	char a;
+//	int b;
+//	char c;
+//};
+////设置过后要进行取消
+//#pragma pack()
+
+//位段
+
+//struct A
+//{
+//	int _a : 2;
+//	int _b : 5;
+//	int _c : 10;
+//	int _d : 30;
+//};
+
+//struct S
+//{
+//	char a : 3;
+//	char b : 4;
+//	char c : 6;
+//	char d : 5;
+//};
+//
+//
+//int main()
+//{
+//	struct S s = { 0 };
+//	s.a = 10;
+//	s.b = 3;
+//	s.c = 12;
+//	s.d = 4;
+//	return 0;
+//}
+
+
+//enum Color
+//{
+//	RED,//0
+//	GREEN,//1
+//	BLUE//2
+//};
+//
+//enum Color
+//{
+//	RED,//0
+//	GREEN = 10,//10
+//	BLUE//11
+//};
+
+//用联合体去判断大小端
+
+//union UN
+//{
+//	int i;
+//	char c;
+//};
+//
+//int check_sys()
+//{
+//	union UN u;
+//	u.i = 1;
+//	return u.c;
+//}
+//
+//int main()
+//{
+//
+//	if (check_sys())
+//	{
+//		printf("小端");
+//	}
+//	else
+//	{
+//		printf("大端");
+//	}
+//	return 0;
+//}
